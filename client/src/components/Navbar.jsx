1bc99536f5786e9b7ea6bec07d7ef02a9f4e7c7d
Lenny-Opicha <ochangoopicha@gmail.com>
2026-03-14 11:37:24 +0300

    Polished  navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
-function Navbar() {
-  return (
-    <nav style={{padding:"10px", background:"#eee"}}>
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/post-item">Post Item</Link> |{" "}
-      <Link to="/profile">Profile</Link> |{" "}
-      <Link to="/requests">Requests</Link> |{" "}
-      <Link to="/login">Login</Link>
+function Navbar(){
+
+  const navStyle = {
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center",
+    padding:"15px 40px",
+    background:"#2c3e50",
+    color:"white"
+  }
+
+  const linksStyle = {
+    display:"flex",
+    gap:"20px"
+  }
+
+  const linkStyle = {
+    color:"white",
+    textDecoration:"none",
+    fontWeight:"500"
+  }
+
+  return(
+
+    <nav style={navStyle}>
+
+      <h2>Declutter254</h2>
+
+      <div style={linksStyle}>
+
+        <Link style={linkStyle} to="/">Home</Link>
+
+        <Link style={linkStyle} to="/post-item">Post Item</Link>
+
+        <Link style={linkStyle} to="/profile">Profile</Link>
+
+        <Link style={linkStyle} to="/requests">Requests</Link>
+
+        <Link style={linkStyle} to="/login">Login</Link>
+
+      </div>
+
     </nav>
-  );
+
+  )
+
 }
 
-export default Navbar; 
+export default Navbar 
